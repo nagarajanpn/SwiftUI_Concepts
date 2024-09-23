691cee5e541b3aeca988b0aad6186687cf8a527f
nagarajanpn <nagarajanpn2011@gmail.com>
2024-09-23 13:40:35 +0530

    base code added

---
+++ Swift_UI_Concepts/Swift_UI_ConceptsTests/Swift_UI_ConceptsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Swift_UI_ConceptsTests.swift
+//  Swift_UI_ConceptsTests
+//
+//  Created by Nagarajan on 23/09/24.
+//  Copyright © 2024 ORGNAGA. All rights reserved.
+//
+
+import XCTest
+@testable import Swift_UI_Concepts
+
+class Swift_UI_ConceptsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
